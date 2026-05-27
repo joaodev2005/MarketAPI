55ba2e25eddcfcaaf0d459e60c329bc205dbfe39
João Victor <123279954+joaodev2005@users.noreply.github.com>
2026-05-27 19:33:19 -0300

    feat: add requests and responses for UserCase

---
+++ src/Shared/MarketAPI.Communication/Responses/ResponseRegisteredUserJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MarketAPI.Communication.Responses;
+
+public class ResponseRegisteredUserJson
+{
+    public string Name { get; set; } = string.Empty;
+    public ResponseTokensJson Tokens { get; set; } = new ResponseTokensJson();
+}
