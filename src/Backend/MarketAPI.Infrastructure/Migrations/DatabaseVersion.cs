80368ac1aa3bcee4fc23a5fb0d59fc594a524afb
João Victor <123279954+joaodev2005@users.noreply.github.com>
2026-06-01 19:59:15 -0300

    feat: add carts migration

--- src/Backend/MarketAPI.Infrastructure/Migrations/DatabaseVersion.cs
+++ src/Backend/MarketAPI.Infrastructure/Migrations/DatabaseVersion.cs
@@ -5,4 +5,5 @@ internal abstract class DatabaseVersion
     internal const int TABLE_USERS = 1;
     internal const int TABLE_CATEGORIES = 2;
     internal const int TABLE_PRODUCTS = 3;
+    internal const int TABLE_CARTS = 4;
 }
---
+++ src/Backend/MarketAPI.Infrastructure/Migrations/Versions/Version0000004.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+
+namespace MarketAPI.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersion.TABLE_CARTS, "Creating Carts and CartItems tables")]
+public class Version0000004 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Carts")
+            .WithColumn("Id").AsGuid().PrimaryKey()
+            .WithColumn("UserId").AsGuid().NotNullable();
+
+        Create.ForeignKey("FK_Carts_Users")
+            .FromTable("Carts").ForeignColumn("UserId")
+            .ToTable("Users").PrimaryColumn("Id");
+
+        Create.Table("CartItems")
+            .WithColumn("Id").AsGuid().PrimaryKey()
+            .WithColumn("CartId").AsGuid().NotNullable()
+            .WithColumn("ProductId").AsGuid().NotNullable()
+            .WithColumn("Quantity").AsInt32().NotNullable();
+
+        Create.ForeignKey("FK_CartItems_Carts")
+            .FromTable("CartItems").ForeignColumn("CartId")
+            .ToTable("Carts").PrimaryColumn("Id");
+
+        Create.ForeignKey("FK_CartItems_Products")
+            .FromTable("CartItems").ForeignColumn("ProductId")
+            .ToTable("Products").PrimaryColumn("Id");
+    }
+}
